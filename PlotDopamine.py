bcf2eba4907b1cb343e68bfc944ed19d28a39ce9
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2015-11-22 17:40:05 +0100

    Adapted PlotDopamine to ANNarchy 4.5.3

--- PlotDopamine.py
+++ PlotDopamine.py
@@ -1,22 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Test the Conditioning phase
-"""
+from ANNarchy import *
 from TimingNetwork import *
 from TrialDefinition import *
 
 # Compile the network
 compile()
 
-# Area to record
-pop = VTA
-area = pop.name
-
 # Record result of learning
-recorded_areas = {pop: 'r'},
-                  ]
+monitor = Monitor(VTA, 'r', start=False)
 
-print 'Starting sensitization...'
+print('Starting sensitization...')
 
 # Definition of the individual US
 sensitization_setup = [
@@ -30,9 +22,9 @@ for trial in range(10):
     sensitization_trial(sensitization_setup)
 
 # Stop learning in the LH->BLA pathway
-LH_BLA.eta = 1000000.0     
+LH_BLA.disable_learning()    
 
-print 'Starting conditioning...'
+print('Starting conditioning...')
 
 # Definition of the CS-US associations
 conditioning_setup = [
@@ -42,90 +34,91 @@ conditioning_setup = [
 ]
 
 # Record trial 1
-start_record(recorded_areas)
+monitor.start()
 conditioning_trial(conditioning_setup)
-recordings = get_record(recorded_areas)
-before_area = recordings[area]['r']['data']
+recordings = monitor.get()
+before_area = recordings['r']
 
 # Perform 4 conditioning trials per association
+monitor.pause()
 for trial in range(4):
     conditioning_trial(conditioning_setup)
 
 # Record trial 5
-start_record(recorded_areas)
+monitor.resume()
 conditioning_trial(conditioning_setup)
-recordings = get_record(recorded_areas)
-during_area = recordings[area]['r']['data']
+recordings = monitor.get()
+during_area = recordings['r']
 
 # Perform 4 conditioning trials per association
+monitor.pause()
 for trial in range(4):
     conditioning_trial(conditioning_setup)
 
 # Record trial 10
-start_record(recorded_areas)
+monitor.resume()
 conditioning_trial(conditioning_setup)
-recordings = get_record(recorded_areas)
-after_area = recordings[area]['r']['data']
+recordings = monitor.get()
+after_area = recordings['r']
 
-print 'Starting reward omission...'
+print('Starting reward omission...')
 
 # Record one omission trial
-start_record(recorded_areas)
 omission_trial(conditioning_setup)
-recordings = get_record(recorded_areas)
-omit_area = recordings[area]['r']['data']
+recordings = monitor.get()
+omit_area = recordings['r']
 
-print 'Analysing results...'
+print('Analysing results...')
 
 # Plot result of learning
 import pylab as plt
 plt.figure()
 # Trial 1
 ax = plt.subplot2grid((4,3),(0, 0))
-ax.plot(before_area[0][:5000])
+ax.plot(before_area[:5000, 0])
 ax.set_title('CS1 - US1')
 ax.set_ylabel('Trial 1')
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(0, 1))
-ax.plot(before_area[0][5000:11000])
+ax.plot(before_area[5000:11000, 0])
 ax.set_title('CS2 - US2')
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(0, 2))
-ax.plot(before_area[0][11000:18000])
+ax.plot(before_area[11000:18000, 0])
 ax.set_ylim([0.0, 1.2])
 ax.set_title('CS3 - US3')
 # Trial 5
 ax = plt.subplot2grid((4,3),(1, 0))
-ax.plot(during_area[0][:5000])
+ax.plot(during_area[:5000, 0])
 ax.set_ylabel('Trial 5')
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(1, 1))
-ax.plot(during_area[0][5000:11000])
+ax.plot(during_area[5000:11000, 0])
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(1, 2))
-ax.plot(during_area[0][11000:18000])
+ax.plot(during_area[11000:18000, 0])
 ax.set_ylim([0.0, 1.2])
 # Trial 10
 ax = plt.subplot2grid((4,3),(2, 0))
-ax.plot(after_area[0][:5000])
+ax.plot(after_area[:5000, 0])
 ax.set_ylabel('Trial 5')
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(2, 1))
-ax.plot(after_area[0][5000:11000])
+ax.plot(after_area[5000:11000, 0])
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(2, 2))
-ax.plot(after_area[0][11000:18000])
+ax.plot(after_area[11000:18000, 0])
 ax.set_ylim([0.0, 1.2])
 # Omission
 ax = plt.subplot2grid((4,3),(3, 0))
-ax.plot(omit_area[0][:5000])
+ax.plot(omit_area[:5000, 0])
 ax.set_ylabel('Reward omission')
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(3, 1))
-ax.plot(omit_area[0][5000:11000])
+ax.plot(omit_area[5000:11000, 0])
 ax.set_ylim([0.0, 1.2])
 ax = plt.subplot2grid((4,3),(3, 2))
-ax.plot(omit_area[0][11000:18000])
+ax.plot(omit_area[11000:18000, 0])
 ax.set_ylim([0.0, 1.2])
 plt.show()
 
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Code for the model described in:
 
-Vitay, J. and Hamker, F. (2014). Timing and expectation of reward: a neuro-computational model of the afferents to the ventral tegmental area. *Frontiers in Neurorobotics* 8(4), doi: 10.3389/fnbot.2014.00004  
+Vitay, J. and Hamker, F. (2014). Timing and expectation of reward: a neuro-computational model of the afferents to the ventral tegmental area. *Frontiers in Neurorobotics* 8(4), [doi:10.3389/fnbot.2014.00004](doi:10.3389/fnbot.2014.00004)  
 
 ## Run the code
 
-The code currently works with ANNarchy 4.5.3.
+The code currently works with ANNarchy 4.5.3 [https://bitbucket.org/annarchy/annarchy](https://bitbucket.org/annarchy/annarchy).
 
 To display the main figures of the article, simply run:
 
@@ -17,3 +17,9 @@ python GenerateFigures.py
 ## Content
 
 * `GenerateFigures.py`: main script to generate the figures. 
+* `PlotDopamine.py`: generates only the figure for dopamine.
+* `TimingNetwork.py`: definition of the network. 
+* `NeuronDefinition.py`: definition of the neurons. 
+* `SynapseDefinition.py`: definition of the synapses. 
+* `ConnectorDefinition.py`: definition of the custom connection patterns. 
+* `TrialDefinition.py`: definition of the trial procedures. 
